8de0e75b1e0a735c9165aed182d99e2cc4d62e54
kabeerbaliyan <kabeerbaliyan@localhost>
2025-02-07 15:22:34 +0530

    Commit

---
+++ servlet-demo/src/main/java/com/java/Welcome.java
@@ -0,0 +1,31 @@
+package com.java;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/kabeer")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+   
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to sevlet");
+		response.getWriter().println("<h1>welcome to the KABEER BALIYAN MAC</h1>");
+	}
+
+}
